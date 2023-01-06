8c8d2c236c03736e2495afab3b86d0a72fe0cbd6
nateclind <iconseq@gmail.com>
2023-01-05 16:13:33 -0800

    ACS prep fixes

--- student_risk/acs_prep.py
+++ student_risk/acs_prep.py
@@ -7,19 +7,21 @@ from osgeo import ogr
 driver = ogr.GetDriverByName("OpenFileGDB")
 
 #%%
-gdb = driver.Open("C:\\Users\\nathan.lindstedt\\Downloads\\ACS_2020_5YR_ZCTA.gdb.zip")
+for year in range(2013, 2020):
 
-with gdb:
-    features = []
+	gdb = driver.Open(f"C:\\Users\\nathan.lindstedt\\Desktop\\shapefiles\\zcta\\ACS_{year}_5YR_ZCTA.gdb.zip")
 
-    for featsClass_idx in range(gdb.GetLayerCount()):
-        featsClass = gdb.GetLayerByIndex(featsClass_idx)
-        features.append(featsClass.GetName())
+	with gdb:
+		features = []
 
-    my_indices = [0, 1, 2, 14, 16, 18, 23, 30]
+		for featsClass_idx in range(gdb.GetLayerCount()):
+			featsClass = gdb.GetLayerByIndex(featsClass_idx)
+			features.append(featsClass.GetName())
 
-    filtered_features = [features[i] for i in my_indices]
+		my_indices = [0, 1, 2, 14, 16, 18, 23, 30]
 
-    for feature in filtered_features:
-        cmd_lst = ["C:\\Program Files\\QGIS 3.16\\bin\\ogr2ogr.exe", "-skipfailures", "-f", "CSV", f"C:\\Users\\nathan.lindstedt\\Desktop\\acs_raw\\acs_2020_5yr_zcta_{feature}.csv", "C:\\Users\\nathan.lindstedt\\Downloads\\ACS_2020_5YR_ZCTA.gdb.zip", f"{feature}"]
-        subprocess.check_call(cmd_lst)
+		filtered_features = [features[i] for i in my_indices]
+
+		for feature in filtered_features:
+			cmd_lst = ["C:\\Program Files\\QGIS 3.16\\bin\\ogr2ogr.exe", "-skipfailures", "-f", "CSV", f"C:\\Users\\nathan.lindstedt\\Desktop\\acs_raw\\acs_{year}_5yr_zcta_{feature}.csv", f"C:\\Users\\nathan.lindstedt\\Downloads\\ACS_{year}_5YR_ZCTA.gdb.zip", f"{feature}"]
+			subprocess.check_call(cmd_lst)
